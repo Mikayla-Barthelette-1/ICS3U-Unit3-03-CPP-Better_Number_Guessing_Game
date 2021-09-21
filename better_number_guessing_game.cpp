780be2a36d62d52698a18fcf1a6e7eb847943b94
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-09-21 23:57:04 +0000

    added better_number_guessing_game.cpp

---
+++ better_number_guessing_game.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2021 Mikayla Barthelette All rights reserved
+//
+// Created by: Mikayla Barthelette
+// Created on: Sept 2021
+// This program creates a better number guessing game
+
+#include <iostream>
+#include <random>
+
+
+int main() {
+    // this function creates the game
+
+
+    int numberGuessed;
+
+    int someRandomNumber;
+
+
+
+    std::random_device rseed;
+
+    std::mt19937 rgen(rseed());  // mersenne_twister
+
+    std::uniform_int_distribution<int> idist(0 , 9);  // [0 , 9]
+
+    someRandomNumber = idist(rgen);
+
+
+    // Input
+    std::cout << "Enter the number between 0 - 9: ";
+    std::cin >> numberGuessed;
+
+    // Process
+    if (numberGuessed == someRandomNumber) {
+        std::cout << "You guessed correct!";
+    } else {
+        std::cout << "Incorrect, the number was " << someRandomNumber;
+    }
+
+    // Output
+    std::cout << "" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
